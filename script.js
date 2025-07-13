c0723a46eeab014598643d9460243e34c8ed7ecd
Lucia Moran <lucia.moran@unmsm.edu.pe>
2025-07-12 19:49:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,9 @@ const sound = document.getElementById('sound');
 for (let i = 1; i <= 24; i++) {
   const box = document.createElement('div');
   box.classList.add('gift-box');
-  box.dataset.index = i;
-
   box.innerHTML = `
-    <div class="ribbon horizontal"></div>
-    <div class="ribbon vertical"></div>
+    <div class="ribbon-horizontal"></div>
+    <div class="ribbon-vertical"></div>
     <div class="bow"></div>
     <div class="box-number">${i}</div>
   `;
@@ -22,15 +20,10 @@ for (let i = 1; i <= 24; i++) {
     modal.style.display = 'flex';
     sound.currentTime = 0;
     sound.play();
-    confetti({
-    particleCount: 100,
-    spread: 70,
-    origin: { y: 0.6 }
-  });
+    confetti({ particleCount: 100, spread: 60, origin: { y: 0.6 } });
   });
 
   giftGrid.appendChild(box);
-}
 
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
