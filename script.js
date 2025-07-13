6d037a7473822d2c5041eff08821decf3b2bb149
Lucia Moran <lucia.moran@unmsm.edu.pe>
2025-07-12 19:44:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,37 @@
-const giftGrid = document.querySelector('.gift-grid');
+const giftGrid = document.getElementById('giftGrid');
 const modal = document.getElementById('modal');
-const modalImage = document.getElementById('modalImage');
-const closeBtn = document.getElementById('closeModal');
+const surpriseImg = document.getElementById('surpriseImg');
+const closeBtn = document.getElementById('closeBtn');
 const sound = document.getElementById('sound');
 
-// Generar cajas con evento
 for (let i = 1; i <= 24; i++) {
   const box = document.createElement('div');
   box.classList.add('gift-box');
-  box.innerText = i;
   box.dataset.index = i;
+
+  box.innerHTML = `
+    <div class="ribbon horizontal"></div>
+    <div class="ribbon vertical"></div>
+    <div class="bow"></div>
+    <div class="box-number">${i}</div>
+  `;
 
   box.addEventListener('click', () => {
     box.classList.add('opened');
-    showModal(i);
-    sound.currentTime = 0; // Reinicia sonido si se repite
+    surpriseImg.src = `img/sorpresa${i}.png`;
+    modal.style.display = 'flex';
+    sound.currentTime = 0;
     sound.play();
+    confetti({
+    particleCount: 100,
+    spread: 70,
+    origin: { y: 0.6 }
+  });
   });
 
   giftGrid.appendChild(box);
 }
 
-// Mostrar modal
-function showModal(index) {
-  modalImage.src = `img/sorpresa${index}.png`;
-  modal.style.display = 'flex';
-
-  const lid = document.getElementById('modalLid');
-  lid.style.animation = 'none';
-  lid.offsetHeight; // forzar reflow
-  lid.style.animation = 'flyUp 1s forwards';
-}
-
-// Cerrar modal
 closeBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
